823f79fa2140b512af2aa3675312f08cf34c14e6
Jacob Andersen <jacobandersen@zipcodersMBP77.home>
2018-11-01 22:39:52 -0400

    methods written tests passed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,113 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    public static void main(String[] args) {
+
+
+
+
+
+        PhoneBook phonebook = new PhoneBook();
+
+        phonebook.add("johnny", "302-555-6789");
+        phonebook.add("lewis black", "244-324-5590");
+        phonebook.add("johnny", "215-344-5645");
+         phonebook.add("sally fields", "214-335-4545");
+        phonebook.add("sally fields", "213-336-4588");
+        phonebook.add("jimbo", "210-2478-1111");
+        phonebook.add("eric idle", "215-2245-3323");
+        System.out.println(phonebook.listNamesAndNumbers());
+
+
+    }
+
+    private Map<String, ArrayList<String>> contacts;
+
+    public PhoneBook() {
+        contacts = new TreeMap<String, ArrayList<String>>();
+
+    }
+
+    public boolean hasEntry(String name) {
+
+        return contacts.containsKey(name);
+    }
+
+    public void add(String name, String phoneNumber) {
+        ArrayList<String> details = new ArrayList<>();
+        if (hasEntry(name)) {
+            details = contacts.get(name);
+        }
+        details.add(phoneNumber);
+        contacts.put(name, details);
+
+    }
+
+    public String lookup(String name) {
+        String s = "";
+        ArrayList<String> pnumber = new ArrayList<>();
+        pnumber = contacts.get(name);
+        //    for (int i = 0; i < pnumber.size(); i++) {
+        //   s += pnumber.get(i);
+        s += pnumber.get(0);
+        return s;
+    }
+
+
+    public String reverseLookup(String phoneNumber) {
+        // if (contacts.containsValue(phoneNumber)) {
+        String status = "person not found!";
+        for (String name : contacts.keySet()) {
+            ArrayList<String> reverse = contacts.get(name);
+            for (String searching : reverse) {
+                if (searching.equals(phoneNumber)) {
+                    status = name;
+                }
+            }
+        }
+        return status;
+    }
+
+    public void remove(String name) {
+        contacts.remove(name);
+    }
+
+    public void removeRecord(String name)
+    {
+        contacts.remove(name);
+    }
+
+    public void removePhoneNumber(String number) {
+        String srcName = reverseLookup(number);
+        ArrayList<String> newPhonelist = new ArrayList<>();
+        ArrayList<String> pnumber = new ArrayList<>();
+        pnumber = contacts.get(srcName);
+        for (int i = 0; i < pnumber.size(); i++) {
+            if (!(pnumber.get(i).equals(number))) {
+                newPhonelist.add(i, pnumber.get(i));
+            }
+            contacts.put(srcName, newPhonelist);
+        }
+    }
+
+    public String listNamesAndNumbers() {
+        {
+            String s = "";
+            //System.out.println(contacts.keySet());
+            for (String name : contacts.keySet()) {
+                ArrayList<String> phoneList = contacts.get(name);
+                for (int i = 0; i < phoneList.size(); i++) {
+                    s += " " + name + " " + " " + phoneList.get(i) + "\n";
+                }
+            }
+            return(s);
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,98 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void hasEntryTest() {
+        //Given
+        PhoneBook book = new PhoneBook();
+        String name = "sally fields";
+        String phonenumber = "214-335-4545";
+        //when
+        book.add(name, phonenumber);
+        boolean actualOutput = book.hasEntry(name);
+        //then
+        Assert.assertTrue(actualOutput);
+    }
+
+    @Test
+    public void addTest() {
+        //Given
+        PhoneBook book = new PhoneBook();
+        String name = "bruce willis";
+        String phonenumber = "123-456-7890";
+        //when
+        book.add(name, phonenumber);
+        String expected = "123-456-7890";
+        String actual = book.lookup(name);
+        Assert.assertEquals(expected, actual);
+        //then
+    }
+
+    @Test
+    public void lookupTest() {
+        PhoneBook book = new PhoneBook();
+        book.add("johnny", "302-555-6789");
+        String name = "johnny";
+
+        String expected = "302-555-6789";
+
+        String actual = book.lookup(name);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookUpTest() {
+        PhoneBook book = new PhoneBook();
+        book.add("charlie chaplin", "123-456-7890");
+
+        String expected = "charlie chaplin";
+
+        String actual = book.reverseLookup("123-456-7890");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest() {
+        PhoneBook book = new PhoneBook();
+        book.add("johnny carson", "124-680-3345");
+        book.removeRecord("johnny carson");
+
+        boolean expected = false;
+
+        boolean actual = book.hasEntry("johnny carson");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeRecord() {
+        PhoneBook book = new PhoneBook();
+        book.add("jimbo", "210-2478-1111");
+        book.removeRecord("jimbo");
+
+        boolean expected = false;
+
+        boolean actual = book.hasEntry("jimbo");
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void removePhoneNumber() {
+        PhoneBook book = new PhoneBook();
+        book.add("jimbo", "210-2478-1111");
+        book.removePhoneNumber("210-2478-1111");
+
+        String expected = "person not found!";
+
+        String actual = book.reverseLookup("210-2478-1111");
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
